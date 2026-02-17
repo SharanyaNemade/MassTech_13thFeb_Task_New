7c16753ae4977ac8feb307063ca0ac9457805f9b
Sharanya Nemade <87638228+SharanyaNemade@users.noreply.github.com>
2026-02-17 21:17:10 +0530

    Add files via upload
    Updated SQL Query with AddEmployees tables

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -443,3 +443,183 @@ BEGIN
     WHERE RoleId = @RoleId;
 END;
 GO
+
+
+
+
+
+/*-----------------------  AddEmp  -----------------------*/
+
+
+
+use Task;
+
+
+CREATE TABLE AddEmployees
+(
+    EmpId INT IDENTITY(1,1) PRIMARY KEY,
+    Name Varchar(100) UNIQUE,
+    Email Varchar(100) UNIQUE,
+    Contact Varchar(20) UNIQUE,
+    Address Varchar(200),
+    Department Varchar(50),
+    Designation Varchar(50),
+    Role Varchar(20) ,
+    Status Varchar(20),
+    ProfilePhoto Varchar(200)
+)
+
+
+
+ALTER TABLE AddEmployees
+ADD Manager VARCHAR(100) NULL
+
+
+
+select * from AddEmployees;
+
+
+
+CREATE PROCEDURE sp_InsertEmployee  
+    @Name VARCHAR(100),  
+    @Email VARCHAR(100),  
+    @Contact VARCHAR(20),  
+    @Address VARCHAR(200),  
+    @Department VARCHAR(50),  
+    @Designation VARCHAR(50),  
+    @Role VARCHAR(20),  
+    @Status VARCHAR(20),  
+    @ProfilePhoto VARCHAR(200)  
+AS  
+BEGIN  
+    INSERT INTO AddEmployees
+    (
+        Name,
+        Email,
+        Contact,
+        Address,
+        Department,
+        Designation,
+        Role,
+        Status,
+        ProfilePhoto
+    )
+    VALUES
+    (
+        @Name,
+        @Email,
+        @Contact,
+        @Address,
+        @Department,
+        @Designation,
+        @Role,
+        @Status,
+        @ProfilePhoto
+    )
+END
+GO
+
+
+
+
+
+
+ALTER PROCEDURE sp_InsertEmployee
+(
+    @Name NVARCHAR(100),
+    @Email NVARCHAR(100),
+    @Contact NVARCHAR(20),
+    @Address NVARCHAR(255),
+    @Department NVARCHAR(100) = NULL,
+    @Designation NVARCHAR(100) = NULL,
+    @Manager NVARCHAR(100) = NULL,
+    @Role NVARCHAR(50),
+    @Status NVARCHAR(50),
+    @ProfilePhoto NVARCHAR(255)
+)
+AS
+BEGIN
+    INSERT INTO AddEmployees
+    (
+        Name,
+        Email,
+        Contact,
+        Address,
+        Department,
+        Designation,
+        Manager,
+        Role,
+        Status,
+        ProfilePhoto
+    )
+    VALUES
+    (
+        @Name,
+        @Email,
+        @Contact,
+        @Address,
+        @Department,
+        @Designation,
+        @Manager,
+        @Role,
+        @Status,
+        @ProfilePhoto
+    )
+END
+GO
+
+
+
+
+
+
+CREATE PROCEDURE sp_GetAllEmployee
+AS
+BEGIN
+    SELECT * FROM AddEmployees
+END
+
+
+
+
+
+CREATE PROCEDURE sp_GetEmployeeById
+    @EmpId INT
+AS
+BEGIN
+    SELECT * FROM Employee WHERE EmpId = @EmpId
+END
+
+
+
+
+CREATE PROCEDURE sp_UpdateEmployee
+    @EmpId INT,
+    @Name VARCHAR(100),
+    @Email VARCHAR(100),
+    @Contact VARCHAR(20),
+    @Address VARCHAR(200),
+    @Department VARCHAR(50),
+    @Designation VARCHAR(50),
+    @Role VARCHAR(20),
+    @Status VARCHAR(20),
+    @ProfilePhoto VARCHAR(200)
+AS
+BEGIN
+    UPDATE AddEmployees
+    SET Name=@Name,
+        Email=@Email,
+        Contact=@Contact,
+        Address=@Address,
+        Department=@Department,
+        Designation=@Designation,
+        Role=@Role,
+        Status=@Status,
+        ProfilePhoto=@ProfilePhoto
+    WHERE EmpId=@EmpId
+END
+
+
+
+
+
